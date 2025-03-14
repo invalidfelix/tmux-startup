9ee2f0069e63d690694f934b90657177167c13d7
invalidfelix <pseowner@gmail.com>
2025-03-14 21:02:48 +0100

    Fix success check with subprocess

--- startup.py
+++ startup.py
@@ -9,17 +9,16 @@ print("------------------------------------")
 time.sleep(5)
 
 
+names = []
+
+
 def run(path):
     name = path.split('/')[-1]
     
     os.system(f"tmux new -d -s '{name}'")
     os.system(f"tmux send-keys -t 'cd {path}' C-m")
-    result = os.system(f"tmux send-keys 'python3 main.py' C-m")
-    
-    if result == 0:
-        print(f"Started {path} successfully")
-    else:
-        print(f"Failed to start {path}")
+    os.system(f"tmux send-keys 'python3 main.py' C-m")
+    names.append(name)
     
     
 home = "../home/"
@@ -27,7 +26,7 @@ home = "../home/"
 
 for dir1 in os.listdir(home):
     if "main.py" not in os.listdir(home + dir1):
-        print(f"Detected {dir1} as a subfolder")
+        print(f"Info: Detected {dir1} as a subfolder")
         for dir2 in os.listdir(home + dir1): # iterate through all subdirectories
             if "main.py" not in os.listdir(home + dir1 + "/" + dir2):
                 print(f"Warning: Detected empty subfolder {dir1}/{dir2}")
@@ -35,3 +34,12 @@ for dir1 in os.listdir(home):
                 run(dir1 + "/" + dir2)
     else:
         run(dir1)
+        
+for name in names:
+    result = subprocess.run(["tmux", "capture-pane", "-pt", name, "-S", "3", "-E", "10"], stdout=subprocess.PIPE)
+    output = result.stdout.decode("utf-8")
+    
+    if "online" in output:
+        print(f"Info: {name} is online")
+    else:
+        print(f"Warning: {name} is offline")
