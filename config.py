4f7558793bcacedfb77a840da9f3f22e2108ee76
invalidfelix <pseowner@gmail.com>
2025-03-15 11:46:41 +0100

    Fix path inside new tmux sessions

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-# The path of the bot directory, startup at the root directory
+# The path to the bot directory, startup at the directory of this script
 bot_directory = "../home/"
 
 # The name of the main file to start every bot
