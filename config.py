2809bb636b5943716f6f485919b310ad9104d6e2
invalidfelix <pseowner@gmail.com>
2025-03-15 11:51:46 +0100

    Add Pelican mode configuration and command loop to startup script

--- config.py
+++ config.py
@@ -3,3 +3,8 @@ bot_directory = "../home/"
 
 # The name of the main file to start every bot
 main = "main.py"
+
+
+# Whether to turn on Pelican mode
+# The tmux image of the following repository can be used: https://github.com/invalidfelix/pelican-images/
+pelican = False
